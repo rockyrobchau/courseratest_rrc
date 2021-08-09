5b8cd34a858e37cdbae89e83131805322fc7159e
rockyrobchau <87996346+rockyrobchau@users.noreply.github.com>
2021-08-09 19:52:59 -0400

    Update script.js

--- module4_solution_RRC/script.js
+++ module4_solution_RRC/script.js
@@ -1,7 +1,7 @@
 
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "RobertC", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "RobertRC", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
